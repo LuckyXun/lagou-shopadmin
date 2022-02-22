64010685a6ad32508a982211b2235dd2e6c273e7
李璕 <8533432+lixun2333@user.noreply.gitee.com>
2022-02-23 00:41:44 +0800

    add

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: XunL
+ * @Date: 2022-02-12 00:46:22
+ * @LastEditTime: 2022-02-13 01:07:08
+ * @Description: file content
+ */
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const a = 2
+createApp(App).mount('#app')
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: XunL
+ * @Date: 2022-02-12 00:46:22
+ * @LastEditTime: 2022-02-23 00:40:39
+ * @Description: file content
+ */
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()]
+})
