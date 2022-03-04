51a2300b0b84a21e2a2d975d18945a599da6a013
XunL <18380422673@163.com>
2022-03-04 11:11:19 +0800

    chore: lint on commitmsg

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: XunL
  * @Date: 2022-02-12 00:46:22
- * @LastEditTime: 2022-03-03 14:57:02
+ * @LastEditTime: 2022-03-03 17:00:31
  * @Description: file content
  */
 import { createApp } from 'vue'
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 /*
  * @Author: XunL
  * @Date: 2022-02-12 00:46:22
- * @LastEditTime: 2022-02-23 00:40:39
+ * @LastEditTime: 2022-03-03 17:25:03
  * @Description: file content
  */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue(), eslintPlugin({
+    cache: false
+  })]
 })
