8ea813b9dc765902caf20053a5801d528d9753ab
XunL <lixun1@chinatelecom.cn>
2022-07-19 17:28:40 +0800

    feat: 拖拽组件

--- src/components/Draggable/index.vue
+++ src/components/Draggable/index.vue
@@ -1,18 +1,31 @@
 <template>
-  <ul id="items">
-    <li>item 1</li>
-    <li>item 2</li>
-    <li>item 3</li>
+  <ul ref="draggableContainer">
+    <slot />
   </ul>
 </template>
 <script setup lang='ts'>
 import Sortable from 'sortablejs'
-import { onMounted } from 'vue'
+import { onMounted, PropType, ref } from 'vue'
 
+const draggableContainer = ref<HTMLDivElement|null>(null)
+const props = defineProps({
+  modelValue: {
+    type: Array,
+    default: () => []
+  },
+  options: {
+    type: Object as PropType<Sortable.Options>,
+    default: () => {}
+  }
+})
+const sortable = ref<Sortable | null>(null)
 onMounted(() => {
-  const el = document.getElementById('items')
-  if (el) {
-    Sortable.create(el)
+  if (draggableContainer.value) {
+    console.log(draggableContainer.value)
+    sortable.value = Sortable.create(draggableContainer.value, {
+      animation: 300,
+      ...props.options
+    })
   }
 })
 </script>
